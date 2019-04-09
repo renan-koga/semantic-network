5b3944ea0f89cad3abf4fd782fb2e0e21f092ac8
renan-koga <renankoga98@gmail.com>
2019-04-09 15:47:53 -0300

    Alterações

--- semantic-network.py
+++ semantic-network.py
@@ -6,75 +6,96 @@ NETWORK = sys.argv[1]
 GRAPH = networkx.DiGraph()
 
 def createGraph():
-    arq = open(NETWORK)
+	arq = open(NETWORK)
 
-    for line in arq:
-        teste = line.split("\n")
-        words = teste[0].split(" ")
-        GRAPH.add_node(words[0])
-        GRAPH.add_node(words[2])
-        GRAPH.add_edge(words[0], words[2], relacao = words[1])
-    # print(GRAPH[words[0]][words[2]]['relacao'])
-    # print(words)
+	for line in arq:
+		teste = line.split("\n")
+		words = teste[0].split(" ")
+		GRAPH.add_node(words[0])
+		GRAPH.add_node(words[2])
+		GRAPH.add_edge(words[0], words[2], relacao = words[1])
+	# print(GRAPH[words[0]][words[2]]['relacao'])
+	# print(words)
 
-    # print(GRAPH.nodes())
-    # print(GRAPH.edges())
-    # print(GRAPH["neymar"]["atacante"]["relacao"])
-    # print(list(GRAPH.successors("atacante")))
+	# print(GRAPH.nodes())
+	# print(GRAPH.edges("neymar"))
+	# print(GRAPH["neymar"]["atacante"]["relacao"])
+	# print(list(GRAPH.successors("atacante")))
 
-    # exit(1)
+	# exit(1)
 
-# neymar tem sola???
-# V ou F???
-
-# neymar tem chuteira
-# chuteira e_um tenis
-
-def checkSentence(node1, link, node2):
+def checkSentence(node1, link, node2, aux_link, cont):
 	logic = False	
-	if GRAPH.has_edge(node1, node2):
-		if GRAPH[node1][node2]["relacao"] == link or GRAPH[node1][node2]["relacao"] == "e_um":
-			return True
+	print(node1 + " :: " + node2)
+	if node1 == node2:
+		return True
 
-	for node in GRAPH.successors(node1):
-		logic = checkSentence(node, link, node2)
+	for aux_node in GRAPH.successors(node1):
+		# for aux_node in GRAPH.successors(node):
+		if GRAPH[node1][aux_node]["relacao"] != "e_um":
+			if cont > 0:
+				return False
+			else:
+				if GRAPH[node1][aux_node]["relacao"] != "tem":
+					cont += 1
+					aux_link = GRAPH[node1][aux_node]["relacao"]
+
+		# print(node1 + " " + link + " " + node)
+		logic = checkSentence(aux_node, link, node2, aux_link, cont)
 
 		if logic:
 			return logic
-
 	return False
-    # else:
-    #     for node in GRAPH.successors(node1):
-    #         logic = checkSentence(node, "e_um", node2)
-    #         if logic:
-    #             return logic
-
-
-
-    # for node in node1:
-    #     if GRAPH.has_edge(node, node2):
-    #         if GRAPH[node][node2] == link:
-    #             return True
 
 # if len(sys.argv) > 2:
 #     TESTE_FILE = sys.argv[2]
 
 if __name__ == "__main__":
-    createGraph()
-    
-    while (True):
-        print("\nDigite uma setença")
-        sentence = input("> ")
+	createGraph()
+	
+	while (True):
+		print("\nDigite uma sentença")
+		sentence = input("> ")
 
-        splitted = sentence.split(" ")
-        node1 = splitted[0]
-        link = splitted[1]
-        node2 = splitted[2]
+		splitted = sentence.split(" ")
+		node1 = splitted[0]
+		link = splitted[1]
+		node2 = splitted[2]
 
-        logic = checkSentence(node1, link, node2)
-        if logic:
-            print("OK!")
-        else:
-            print("Deu Ruim!")
-        
-        # exit(1)
+		paths = networkx.all_simple_paths(GRAPH, source=node1, target=node2)
+		print(list(paths))
+		exit(1)
+
+		logic = True
+		e_um = False
+		tem = False
+		others = False
+		for path in paths:
+			for i in range(len(path) - 1):
+				node1 = path[i]
+				node2 = path[i+1]
+				if GRAPH[node1][node2]["relacao"] == "e_um":
+					e_um = True
+				elif GRAPH[node1][node2]["relacao"] == "tem":
+					tem = True
+				else:
+					if not others:
+						others = True
+					else:
+						logic = False
+						break
+			# print(path)
+
+		if logic:
+			print("BOA CARAI")
+
+
+		exit(1)
+
+		logic = checkSentence(node1, link, node2, "", 0)
+		if logic:
+			print("OK!")
+		else:
+			print("Deu Ruim!")
+		
+		# exit(1)
