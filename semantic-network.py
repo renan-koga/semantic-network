1b78a285d7e9ea714a077a2c84985919b052da66
Vitor Hugo <custodio_vitor@outlook.com>
2019-04-10 21:46:19 -0300

    Validação do Trabalho
    Está quase tudo funcionando, dei uma polida pra deixar no jeito pra entrega, acho que só falta resolver esse bug mesmo - "neymar e_um tite"

--- semantic-network.py
+++ semantic-network.py
@@ -1,135 +1,68 @@
 import sys
 import networkx
 
-TESTE_FILE = ""
-NETWORK = sys.argv[1]
-GRAPH = networkx.DiGraph()
-
+#Gera a rede semântica a partir do arquivo de entrada
 def createGraph():
 	arq = open(NETWORK)
-
 	for line in arq:
 		teste = line.split("\n")
 		words = teste[0].split(" ")
 		GRAPH.add_node(words[0])
 		GRAPH.add_node(words[2])
 		GRAPH.add_edge(words[0], words[2], relacao = words[1])
-	# print(GRAPH[words[0]][words[2]]['relacao'])
-	# print(words)
 
-	# print(GRAPH.nodes())
-	# print(GRAPH.edges("neymar"))
-	# print(GRAPH["neymar"]["atacante"]["relacao"])
-	# print(list(GRAPH.successors("atacante")))
-
-	# exit(1)
-
-def checkSentence(node1, link, node2, aux_link, cont):
-	logic = False	
-	print(node1 + " :: " + node2)
-	if node1 == node2:
-		return True
-
-	for aux_node in GRAPH.successors(node1):
-		# for aux_node in GRAPH.successors(node):
-		if GRAPH[node1][aux_node]["relacao"] != "e_um":
-			if cont > 0:
-				return False
-			else:
-				if GRAPH[node1][aux_node]["relacao"] != "tem":
-					cont += 1
-					aux_link = GRAPH[node1][aux_node]["relacao"]
-
-		# print(node1 + " " + link + " " + node)
-		logic = checkSentence(aux_node, link, node2, aux_link, cont)
-
-		if logic:
-			return logic
-	return False
-
-# if len(sys.argv) > 2:
-#     TESTE_FILE = sys.argv[2]
-
+NETWORK = sys.argv[1]
+GRAPH = networkx.DiGraph()
 if __name__ == "__main__":
-	createGraph()
-	
+	#GRAPH: DiGrafo -- Gerando o grafo a partir do arquivo de entrada
+	createGraph()	
 	while (True):
 		print("\nDigite uma sentença")
 		sentence = input("> ")
-
+		#Obtendo a sentença a ser analisada
 		splitted = sentence.split(" ")
 		node1 = splitted[0]
 		link = splitted[1]
 		node2 = splitted[2]
-
-		paths = networkx.all_simple_paths(GRAPH, source=node1, target=node2)
-		# print(list(paths))
-		# exit(1)
-
-		logic = True
-		e_um = False
-		tem = False
-		others = False
-		for path in paths:
-			once = False
-			for i in range(len(path) - 1):
-				node1 = path[i]
-				node2 = path[i+1]
-				aux_link = GRAPH[node1][node2]["relacao"]
-
-				if link == "e_um":
-					if aux_link != "e_um":
-						logic = False
-				elif link == "tem":
-					if aux_link != "e_um" and aux_link != "tem":
-						logic = False
-				else:
-					if aux_link != "e_um":
-						if aux_link == link:
-							if once:
+		#Paths: Generator -- Obtendo os caminhos possíveis de origem -> destino (tipo: Generator)
+		try:
+			paths = networkx.all_simple_paths(GRAPH, source=node1, target=node2)
+			if not paths:
+				print("Falso")
+			else:
+				logic = True
+				e_um = False
+				tem = False
+				others = False
+				#Iterando por entre os caminhos existentes de origem -> destino
+				for path in paths:
+					once = False
+					#Comparando os nós de cada caminho de par em par de origem -> destino
+					for i in range(len(path) - 1):
+						node1 = path[i]
+						node2 = path[i+1]
+						aux_link = GRAPH[node1][node2]["relacao"]
+						#Analisando o tipo de relação de cada par de nós
+						#Permutando valores booleanos dependendo da série de relações origem -> destino
+						if link == "e_um":
+							if aux_link != "e_um":
 								logic = False
-							else:
-								once = True
+						elif link == "tem":
+							if aux_link != "e_um" and aux_link != "tem":
+								logic = False
 						else:
-							logic = False
-
-				# if others:
-				# 	if link != "e_um":
-				# 		logic = False
-				
-				# else:
-				# 	if link == "e_um":
-				# 		e_um = True
-				# 	elif link == "tem":
-				# 		tem = True
-				# 	else:
-				# 		others = True
-
-				# if GRAPH[node1][node2]["relacao"] == "e_um":
-				# 	e_um = True
-				# elif GRAPH[node1][node2]["relacao"] == "tem":
-				# 	tem = True
-				# else:
-				# 	if not others:
-				# 		others = True
-				# 	else:
-				# 		logic = False
-				# 		break
-			# print(path)
-
-			if logic:
-				print("Verdadeiro")
-				break
-
-		if not logic:
-			print("Falso")
-
-		# exit(1)
-
-		# logic = checkSentence(node1, link, node2, "", 0)
-		# if logic:
-		# 	print("OK!")
-		# else:
-		# 	print("Deu Ruim!")
-		
-		# exit(1)
+							if aux_link != "e_um":
+								if aux_link == link:
+									if once:
+										logic = False
+									else:
+										once = True
+								else:
+									logic = False
+					if logic:
+						print("Verdadeiro")
+						break
+				if not logic:
+					print("Falso")
+		except:
+			print('Rede Semântica: Erro na busca')
