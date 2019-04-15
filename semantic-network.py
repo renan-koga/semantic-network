b8fd59d25788f7f9781314bedbe83b73c3860fb4
renan-koga <renankoga98@gmail.com>
2019-04-15 17:20:10 -0300

    Correções finais

--- semantic-network.py
+++ semantic-network.py
@@ -1,49 +1,47 @@
 import sys
-import networkx
+from Graph import Graph
 
-#Gera a rede semântica a partir do arquivo de entrada
+TESTE_FILE = ""
+NETWORK = sys.argv[1]
+GRAPH = Graph()
+
 def createGraph():
 	arq = open(NETWORK)
+
 	for line in arq:
 		teste = line.split("\n")
 		words = teste[0].split(" ")
-		GRAPH.add_node(words[0])
-		GRAPH.add_node(words[2])
-		GRAPH.add_edge(words[0], words[2], relacao = words[1])
+		GRAPH.addNode(words[0])
+		GRAPH.addNode(words[2])
+		GRAPH.addEdge(words[0], words[2], words[1])
 
-NETWORK = sys.argv[1]
-GRAPH = networkx.DiGraph()
 if __name__ == "__main__":
-	#GRAPH: DiGrafo -- Gerando o grafo a partir do arquivo de entrada
-	createGraph()	
+	createGraph()
+
 	while (True):
 		print("\nDigite uma sentença")
 		sentence = input("> ")
-		#Obtendo a sentença a ser analisada
+
+		if sentence == "quit":
+			break
+
 		splitted = sentence.split(" ")
 		node1 = splitted[0]
 		link = splitted[1]
 		node2 = splitted[2]
-		#Paths: Generator -- Obtendo os caminhos possíveis de origem -> destino (tipo: Generator)
-		try:
-			paths = networkx.all_simple_paths(GRAPH, source=node1, target=node2)
-			if not paths:
-				print("Falso")
-			else:
-				logic = True
-				e_um = False
-				tem = False
-				others = False
-				#Iterando por entre os caminhos existentes de origem -> destino
+
+		paths = GRAPH.getPath(node1, node2)
+
+		logic = True
+		if paths:
+			if len(paths) > 0:
 				for path in paths:
 					once = False
-					#Comparando os nós de cada caminho de par em par de origem -> destino
 					for i in range(len(path) - 1):
 						node1 = path[i]
 						node2 = path[i+1]
-						aux_link = GRAPH[node1][node2]["relacao"]
-						#Analisando o tipo de relação de cada par de nós
-						#Permutando valores booleanos dependendo da série de relações origem -> destino
+						aux_link = GRAPH.getEdge(node1, node2)
+
 						if link == "e_um":
 							if aux_link != "e_um":
 								logic = False
@@ -59,10 +57,18 @@ if __name__ == "__main__":
 										once = True
 								else:
 									logic = False
+
 					if logic:
-						print("Verdadeiro")
+						print("Verdadeiro\n")
+						for i in range(len(path) - 1):
+							node1 = path[i]
+							node2 = path[i+1]
+							aux_link = GRAPH.getEdge(node1, node2)
+
+							print(node1 + " " + aux_link + " " + node2)
+
 						break
+
 				if not logic:
 					print("Falso")
-		except:
-			print('Rede Semântica: Erro na busca')
+			else: print("Falso")
